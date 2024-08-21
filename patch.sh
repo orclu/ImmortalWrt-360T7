94a163770100a54f7233f480397ae67e538ce9c8
louiswi <lu814478913@gmail.com>
2024-08-21 22:26:12 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -7,5 +7,8 @@ rm -rf ./feeds/packages/net/v2ray-core && cp -r -f ./feeds/small/v2ray-core ./fe
 rm -rf ./feeds/packages/net/v2ray-plugin && cp -r -f ./feeds/small/v2ray-plugin ./feeds/packages/net/v2ray-plugin
 rm -rf ./feeds/packages/net/v2ray-geodata && cp -r -f ./feeds/small/v2ray-geodata ./feeds/packages/net/v2ray-geodata
 
+rm -rf ./feeds/packages/net/xray-core && cp -r -f ./feeds/small/xray-core ./feeds/packages/net/xray-core
+rm -rf ./feeds/packages/net/xray-plugin && cp -r -f ./feeds/small/xray-plugin ./feeds/packages/net/xray-plugin
+
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
