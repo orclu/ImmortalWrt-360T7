f0b0af68fb1b1d32d9d00da02ef4d1797e7a8578
louiswi <lu814478913@gmail.com>
2024-08-21 13:08:24 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,3 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # 固件版本名称自定义
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By lulu $(date +"%Y%m%d") '/g" package/base-files/files/etc/openwrt_release
 
-rm -rf ./feeds/packages/net/mosdns
