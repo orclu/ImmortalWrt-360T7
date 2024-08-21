d4e78a984a44dc79491557d19baf26caf6cf384d
louiswi <lu814478913@gmail.com>
2024-08-21 18:59:18 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 rm -rf ./feeds/packages/net/mosdns && cp -r -f ./feeds/small/mosdns ./feeds/packages/net/mosdns
+
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
