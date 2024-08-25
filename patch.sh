c3fbaae9038b4127ef84a843a52f6d3c2f757768
louiswi <lu814478913@gmail.com>
2024-08-25 15:27:36 +0800

    更新 patch.sh

--- patch.sh
+++ patch.sh
@@ -3,6 +3,13 @@ rm -rf ./feeds/packages/net/mosdns && cp -r -f ./feeds/small/mosdns ./feeds/pack
 
 rm -rf ./feeds/packages/net/hysteria && cp -r -f ./feeds/small/hysteria ./feeds/packages/net/hysteria
 
+shadowsocksr-libev
+shadowsocks-rust
+
+dns2tcp
+ipt2socks
+shadow-tls
+
 rm -rf ./feeds/packages/net/v2ray-core && cp -r -f ./feeds/small/v2ray-core ./feeds/packages/net/v2ray-core
 rm -rf ./feeds/packages/net/v2ray-plugin && cp -r -f ./feeds/small/v2ray-plugin ./feeds/packages/net/v2ray-plugin
 rm -rf ./feeds/packages/net/v2ray-geodata && cp -r -f ./feeds/small/v2ray-geodata ./feeds/packages/net/v2ray-geodata
